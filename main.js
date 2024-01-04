ff571f2e96fafea084d57e1871a87b34bcc1d34a
dexteranthonyremegiogithub@gmail.com <dexteranthonyremegiogithub@gmail.com>
2024-01-05 02:27:19 +0800

    FirstCommit

---
+++ main.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+  $(".about").click(function(){
+    $(".abouts").slideToggle(3000);
+  });
+});
+
+$(document).ready(function(){
+  $(".contact").click(function(){
+    $(".contacts").slideToggle(3000);
+  })
+});
+
+$(document).ready(function(){
+  $(".button1").click(function(){
+   $(".spaghetti").slideToggle(2000);
+  });
+});
+
+$(document).ready(function(){
+  $(".button2").click(function(){
+    $(".tinola").slideToggle(2000);
+  });
+});
+
+$(document).ready(function(){
+  $(".button3").click(function(){
+   $(".sinigang").slideToggle(2000);
+  })
+})
