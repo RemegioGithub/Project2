96533aa82c1f48bb7d99e0a0b19e0344df2937f1
dexteranthonyremegiogithub@gmail.com <dexteranthonyremegiogithub@gmail.com>
2024-01-10 12:53:58 +0800

    Project5

---
+++ Portfolio/scriptFortfolio.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $(".btn-primary").click(function(){
+    $(".well-sm").slideToggle(2000);
+  });
+});
+
+$(document).ready(function(){
+  $(".course").click(function(){
+    $(".courses").slideToggle(2000);
+  });
+});
+
+$(document).ready(function(){
+  $(".skill").click(function(){
+    $(".skills").slideToggle(2000);
+  });
+});
+
+$(document).ready(function(){
+  $(".contact").click(function(){
+    $(".contacts").slideToggle(2000);
+  });
+});
